5263c950efffabf1e4917ad9e812015a4581d011
Assaf Assa <assafas141@gmail.com>
2023-12-07 22:23:57 +0200

    make it better

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const authController= require('./controllers/authcontrollers')
 
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(3000))
+    .then((result) => app.listen(process.env.PORT || 3000))
     .catch((err) => console.log(err))
 
 app.set('view engine', 'ejs')
